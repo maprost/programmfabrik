5d73406826f5f1aa3a488a2fa87f4c532d044ee9
Matthias <matthias.leuthaeuser@gmx.de>
2025-02-17 18:42:17 +0100

    fix json list bug

--- internal/handler.go
+++ internal/handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type JsonTag struct {
@@ -31,22 +32,26 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 
 func runHandler(w http.ResponseWriter, r *http.Request, filter string) {
 	c := make(chan JsonTable, 1)
-	quit := make(chan struct{})
+	quitFlush := make(chan struct{})
+	quitCheck := make(chan struct{})
+	realQuit := make(chan struct{})
 	canceled := false
-	go flushJson(w, c, quit)
-	go checkCancelStatus(r, quit, &canceled)
+	go flushJson(w, c, quitFlush, realQuit)
+	go checkCancelStatus(r, quitCheck, &canceled)
 
 	err := callExiftool(c, &canceled, filter)
 
-	quit <- struct{}{}
+	quitFlush <- struct{}{}
+	quitCheck <- struct{}{}
+
+	<-realQuit
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println("request done")
 }
 
-func flushJson(w http.ResponseWriter, c chan JsonTable, quit chan struct{}) {
+func flushJson(w http.ResponseWriter, c chan JsonTable, quit chan struct{}, realQuit chan struct{}) {
 	flusher, ok := w.(http.Flusher)
 	if !ok {
 		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
@@ -56,22 +61,43 @@ func flushJson(w http.ResponseWriter, c chan JsonTable, quit chan struct{}) {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
+	fmt.Fprint(w, "[") // simulating a json list
+	sendOne := false
+	flush := func(t JsonTable) {
+		if sendOne {
+			fmt.Fprint(w, ",")
+		} else {
+			sendOne = true
+		}
+		err := json.NewEncoder(w).Encode(t)
+		if err != nil {
+			fmt.Println("json error: ", err)
+			sendOne = false // so there are no two comma in line
+		} else {
+			flusher.Flush()
+		}
+	}
+
 	var t JsonTable
 	for {
 		select {
 		case <-quit:
-			//fmt.Println("quit")
-			return
+			timer := time.NewTimer(1 * time.Second) // if quit channel is faster than c channel, wait a second before finish it.
+			for {
+				select {
+				case <-timer.C:
+					fmt.Fprint(w, "]")
+					flusher.Flush()
+					realQuit <- struct{}{}
+					return
+
+				case t = <-c:
+					flush(t)
+				}
+			}
 
 		case t = <-c:
-			err := json.NewEncoder(w).Encode(t)
-			if err != nil {
-				fmt.Println("json error: ", err)
-				continue
-			} else {
-				// TODO: flush should also work with a list
-				flusher.Flush()
-			}
+			flush(t)
 		}
 	}
 }
@@ -79,13 +105,12 @@ func flushJson(w http.ResponseWriter, c chan JsonTable, quit chan struct{}) {
 func checkCancelStatus(r *http.Request, quit chan struct{}, canceled *bool) {
 	ctx := r.Context()
 
-	for {
+	for !*canceled {
 		select {
 		case <-quit:
 			return
 		case <-ctx.Done():
 			*canceled = true
-			return
 		}
 	}
 }
